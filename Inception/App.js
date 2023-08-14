a1562dee6b0cea8faff9db54298bf21f3875a5ec
Rishabh Kunwar <kunwarrishabh70@gmail.com>
2023-08-14 13:04:32 +0530

    creating nested element in react

---
+++ Inception/App.js
@@ -0,0 +1,9 @@
+const element = React.createElement("div", { className: "main" }, [
+  React.createElement("h1", { id: "head1" }, "Hello World From nested H1"),
+  React.createElement("p", { id: "para1" }, "Hello from nested sibling P"),
+]);
+
+console.log(element); //remember it is an react element which is a javascript object and render() function will convert it to HTML element and print it on screen
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
